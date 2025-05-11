0c3553efa74e95dac325c1bfffcd2fd4f5b12dba
arfoysal <arfoysal.me@gmail.com>
2025-05-11 23:20:26 +0600

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class AppTest extends DriverSetup{
+
+    @Test
+    public void testWebPageLoad() {
+       driver.get("https://google.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+
+    @Test
+    public void testWebPageTitle() {
+        driver.get("https://google.com");
+        System.out.println(driver.getTitle());
+    }
+
+
+}
